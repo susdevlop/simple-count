407238e444add3c6507ea541316544c5e9fedf26
susdevlop <ilovemm520@vip.qq.com>
2019-02-07 16:14:27 +0800

    Add files via upload

--- count.js
+++ count.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    var computer= "0";
+    // var computer= "0";
     var num = document.getElementById("num");
     var cR = document.getElementById("countResult");
     var Abutton = new Array();
@@ -70,7 +70,38 @@ function secondDo(){
     var cR = document.getElementById("countResult");
     cR.innerHTML="";
 }
+//    \uxxxx	查找以十六进制数 xxxx 规定的 Unicode 字符。
+// U+002B + 加号 
+// U+002D - 连字号/减号 
+// U+002A * 星号 
+// U+002F / 由右上至左下的斜线 
+// U+0025 % 百分比符号 
+/**
+ * substr() 方法可在字符串中抽取从 start 下标开始的指定数目的字符。
+ const name =[{1:"张三"},{2:"李四哥"},{3:"王二哥哥"}];
+返回， 三 、四哥 、哥哥，这样的效果就ok
+第一种：
+"王二弟弟".match(/.+(.{1,2})$/);
+第二种：
+'啥名字呵呵'.substr(1).substr(-2);
+ */
+// 因为学习正则表达式原因，此处计划运用 \u 和直接反斜杠加符号
+//设置一个判断符号是否为结尾的正则表达式函数 当匹配到结尾为符号时，如果点击
+//doCount函数，就替换符号
+// (x|y)	查找任何以 | 分隔的选项。
+// var patt = /e/;
+// patt.test("The best things in life are free!");
+function ifExist(){
+    var sourceProblem = num.innerText;
+    console.log("sourceproblem的值为："+sourceProblem);
+    var patt = /[\+\-\u002A\u002f\%]$/;
+    if(patt.test(sourceProblem)){
+        sourceProblem = sourceProblem.replace(patt,"");
+        num.innerHTML=sourceProblem;
+    }
+}
 function doCount(){
+    ifExist();
     if(num.innerText==""){
         var cR = document.getElementById("countResult");
         var cRsource = cR.innerText;
@@ -81,19 +112,19 @@ function doCount(){
     num.innerHTML=newValue;
     secondDo();
 }
-
-function dector(){
-    var computer="0";
-    if(computer=="0"){
-        alert("pls open the computer");
-    }
-};
-function open(){
-    var computer = "1";
-};
-function turnOff(){
-    var computer = "0";
-};
 function show(t){
     num.innerHTML=num.innerText+t;
 }
+
+// function dector(){
+//     var computer="0";
+//     if(computer=="0"){
+//         alert("pls open the computer");
+//     }
+// };
+// function open(){
+//     var computer = "1";
+// };
+// function turnOff(){
+//     var computer = "0";
+// };
